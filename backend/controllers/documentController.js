08cdc909057992304034833a583f71fcd32a37ed
selvaganeshu <uselvaganesh6@gmail.com>
2026-01-07 08:05:27 +0530

    Add API to list uploaded documents for user

--- backend/controllers/documentController.js
+++ backend/controllers/documentController.js
@@ -37,3 +37,21 @@ export const uploadDocument = async(req,res)=>{
         })
     }
 }
+
+export const getDocuments = async(req,res)=>{
+    try{
+        const userId = req.user._id;
+        const documents = await Document.find({userId}).sort({createdAt : -1});
+        res.status(200).json({
+            success : true,
+            data : documents
+        })
+    }
+    catch(error){
+        console.error(`error : ${error}`);
+        res.status(500).json({
+            success : false,
+            error : "Server Error"
+        })
+    }
+}
--- backend/routes/documentRoutes.js
+++ backend/routes/documentRoutes.js
@@ -1,4 +1,4 @@
-import { uploadDocument } from "../controllers/documentController.js";
+import { uploadDocument,getDocuments } from "../controllers/documentController.js";
 import express from "express";
 import upload from "../config/multer.js";
 import protect from "../middleware/authMiddleware.js";
@@ -11,6 +11,6 @@ router.post(
   upload.single("document"),
   uploadDocument
 );
-
+router.get("/", protect, getDocuments);
 
 export default router;
