82a1949d5208c1347d0c395a8091127d2c5094e9
selvaganeshu <uselvaganesh6@gmail.com>
2026-01-18 21:43:06 +0530

    Add API to fetch best quiz score

--- backend/routes/quizAttemptRoutes.js
+++ backend/routes/quizAttemptRoutes.js
@@ -3,7 +3,6 @@ import express from "express";
 import protect from "../middleware/authMiddleware.js";
 
 const router = express.Router();
-
 router.get("/best",protect,getBestQuizScore);
 
 router.post("/:documentId",protect,saveQuizAttempt);
